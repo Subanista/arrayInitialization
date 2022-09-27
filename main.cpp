b0293bb6f8b875ed47d1c7ace7d623c924531e8f
Subanista Thaneswaran <88665143+Subanista@users.noreply.github.com>
2022-09-27 10:33:11 +0000

    added the 2D initialation changed

--- main.cpp
+++ main.cpp
@@ -11,24 +11,32 @@ int main() {
     
     std::cin >> a[i];
   }
-  std::cout << "print the values of an array\n";
+  std::cout << "print the values of 1D array\n";
   for(i=0;i<n;i++){
    std::cout << a[i];
     std::cout<< " \n";
-
+    }
 
     int m,j,k,b[3][3];
     std::cout << "runtime initialization for 2D in array \n";
-    std::cout << "enter the number of elements \n";
-  std::cin >> m;
+    
    std::cout << "enter the  elements to the 2D array \n"; 
-    for(j=0;j<m;j++)
+    for(j=0;j<3;j++)
       {
-      for(k=0;k<m;k++){
+      for(k=0;k<3;k++){
         std::cin>>b[j][k];
       }
         }
+  std::cout << "print the values of 2D array\n";
+ for(j=0;j<3;j++)
+      {
+      for(k=0;k<3;k++){
+        std::cout<<b[j][k];
+        
+      }
+        std:: cout<< " \n";
+        }
 
     
-  }
+  
 }
